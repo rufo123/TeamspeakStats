dfed87ebee571e1f63e694e43e4bee487422d578
Rufo123 <imorudolf@gmail.com>
2023-08-11 14:36:28 +0200

    -- date format pipe and service code edited, to be more accurate -- adjusted and fixed correspoding unit tests -- added unit test to theme.service.spec.ts to test getTheme() method

--- TeamspeakStatsNew/TeamspeakStatsNew/ClientApp/src/app/pipes/date-format.pipe.ts
+++ TeamspeakStatsNew/TeamspeakStatsNew/ClientApp/src/app/pipes/date-format.pipe.ts
@@ -15,49 +15,58 @@ export class DateFormatPipe implements PipeTransform {
         const date = moment.tz(dateStr, timezone);
         const now = moment.tz(timezone);
 
-        const diff = now.diff(date, "milliseconds", true);
+        const duration = moment.duration(now.diff(date));
+        const milliseconds = duration.asMilliseconds();
 
-        const seconds = Math.floor(diff / 1000);
-        const minutes = Math.floor(seconds / 60);
-        const hours = Math.floor(minutes / 60);
-        const days = Math.floor(hours / 24);
-        const months = Math.floor(days / 30);
-        const years = Math.floor(months / 12);
-
-        if (seconds < 60) {
-            return `${seconds} second${seconds === 1 ? "" : "s"} ago`;
-        } else if (minutes < 60) {
-            const remainingSeconds = seconds % 60;
-            return `${minutes} minute${
-                minutes === 1 ? "" : "s"
-            } and ${remainingSeconds} second${
-                remainingSeconds === 1 ? "" : "s"
-            } ago`;
-        } else if (hours < 24) {
-            const remainingMinutes = minutes % 60;
-            return `${hours} hour${
-                hours === 1 ? "" : "s"
-            } and ${remainingMinutes} minute${
-                remainingMinutes === 1 ? "" : "s"
-            } ago`;
-        } else if (days < 30) {
-            const remainingHours = hours % 24;
-            return `${days} day${
-                days === 1 ? "" : "s"
-            } and ${remainingHours} hour${remainingHours === 1 ? "" : "s"} ago`;
-        } else if (months < 12) {
-            const remainingDays = days % 30;
-            return `${months} month${
-                months === 1 ? "" : "s"
-            } and ${remainingDays} day${remainingDays === 1 ? "" : "s"} ago`;
-        } else {
-            const remainingMonths = months % 12;
-            return `${years} year${
-                years === 1 ? "" : "s"
-            } and ${remainingMonths} month${
-                remainingMonths === 1 ? "" : "s"
-            } ago`;
+        // Handle negative durations (future dates)
+        if (milliseconds < 0) {
+            return "Just now";
         }
+
+        const units: (
+            | "years"
+            | "months"
+            | "days"
+            | "hours"
+            | "minutes"
+            | "seconds"
+        )[] = ["years", "months", "days", "hours", "minutes", "seconds"];
+
+        for (let i = 0; i < units.length; i++) {
+            const unit = units[i];
+            const diff = now.diff(date, unit);
+            if (diff > 0) {
+                const diffLabel = diff === 1 ? unit.slice(0, -1) : unit;
+                const nextUnit = units[i + 1];
+
+                const durationNumber = duration.as(unit);
+
+                if (i < 5) {
+                    let remainingInUnitFloored: number =
+                        duration.as(unit) - Math.floor(duration.as(unit));
+
+                    if (remainingInUnitFloored === durationNumber) {
+                        remainingInUnitFloored = 0;
+                    }
+
+                    const remainingInUnit = Math.round(
+                        moment
+                            .duration(remainingInUnitFloored, unit)
+                            .as(nextUnit)
+                    );
+
+                    const remainingLabel =
+                        remainingInUnit === 1
+                            ? nextUnit.slice(0, -1)
+                            : nextUnit;
+
+                    return `${diff} ${diffLabel} and ${remainingInUnit} ${remainingLabel} ago`;
+                }
+
+                return `${diff} ${diffLabel} ago`;
+            }
+        }
+        return `Just now`;
     }
 
     toggleShouldUpdateView() {
